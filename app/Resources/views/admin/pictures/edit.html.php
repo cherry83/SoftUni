f60243ea2960db352f8db43acd835dcc5bdef31a
cherry83 <cherry.83@abv.bg>
2016-12-22 03:22:47 +0200

    Correction

--- app/Resources/views/admin/pictures/edit.html.php
+++ app/Resources/views/admin/pictures/edit.html.php
@@ -1,19 +1,18 @@
 
-            <form class="form-horizontal" action="/admin/categories/edit/<?=$category['id']?>" method="POST">
+            <form class="form-horizontal" action="/admin/pictures/edit/<?=$picture['id']?>" method="POST">
                 <fieldset>
-                    <legend>Промяна на категория</legend>
+                    <legend>Промяна на картинката</legend>
 
                     <div class="form-group">
-                        <label class="col-sm-4 control-label" for="article_title">Име на категорията</label>
+                        <label class="col-sm-4 control-label" for="title">Име на категорията</label>
                         <div class="col-sm-4 ">
-                            <input type="text" class="form-control" id="article_title" placeholder="Име" name="category[name]" value="<?=$category['name']?>" required>
-	                        <?=$category_error?>
+                            <input type="text" class="form-control" id="title" placeholder="Име" name="picture[name]" value="<?=$picture['title']?>" required>
                         </div>
                     </div>
 
                     <div class="form-group">
                         <div class="col-sm-4 col-sm-offset-4">
-                            <a class="btn btn-default" href="/admin/categories">Откажи</a>
+                            <a class="btn btn-default" href="/admin/pictures">Откажи</a>
                             <button type="submit" class="btn btn-success">Запиши</button>
                         </div>
                     </div>
